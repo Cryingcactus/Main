a75d596b1c874df06236226ba2e0937628a3ad4f
Cryingcactus <46989113+Cryingcactus@users.noreply.github.com>
2019-01-24 04:10:59 -0500

    Update copy.js

--- copy.js
+++ copy.js
@@ -1,26 +1,5 @@
 [
   {
-    "name": "Meowsy",
-    "species" : "cat",
-    "foods": {
-      "likes": ["tuna", "catnip"],
-      "dislikes": ["ham", "zucchini"]
-    }
-  },
-  {
-    "name": "Barky",
-    "species" : "dog",
-    "foods": {
-      "likes": ["bones", "carrots"],
-      "dislikes": ["tuna"]
-    }
-  },
-  {
-    "name": "Purrpaws",
-    "species" : "cat",
-    "foods": {
-      "likes": ["mice"],
-      "dislikes": ["cookies"]
-    }
+    "link": "https://www.youtube.com/embed/5NVzgVwB6eY"
   }
 ]
